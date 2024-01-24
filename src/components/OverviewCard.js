be6cde68ca9f8969dc018c81de3e1613b474ba20
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-01-25 02:32:38 +0530

    create a component to show data in overview section

---
+++ src/components/OverviewCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const OverviewCard = ({ name, value, href }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="w-60 px-7 py-5 bg-gray-200 rounded-2xl hover:bg-gray-300 transition-all hover:scale-105 hover:shadow-lg">
+      <div className="flex justify-between items-center">
+        <p className="font-semibold text-xl text-gray-500 text-balance">
+          {name}
+        </p>
+        <button
+          className="bg-gray-200 hover:bg-gray-400 p-2 rounded-xl"
+          onClick={() => navigate(href)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="mt-2">
+        <p className="font-semibold text-5xl text-gray-700">{value}</p>
+      </div>
+    </div>
+  );
+};
+
+export default OverviewCard;
--- src/pages/overview.js
+++ src/pages/overview.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import OverviewCard from '../components/OverviewCard';
+import { UseSelector, useSelector } from 'react-redux';
 
-const overview = () => {
-  return <div>overvfgfgfiew</div>;
+const dummyData = [
+  {
+    key: 1,
+    name: 'Order',
+    value: 201,
+    href: '/order',
+  },
+  {
+    key: 2,
+    name: 'Pending Payments',
+    value: 100,
+    href: '/product',
+  },
+  {
+    key: 3,
+    name: 'Users',
+    value: 50,
+    href: '/user',
+  },
+  {
+    key: 4,
+    name: 'Product Count',
+    value: 20,
+    href: '/product',
+  },
+];
+
+const Overview = () => {
+  const { currentUser } = useSelector((state) => state.auth);
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex flex-row justify-between items-center w-full">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-2xl font-semibold leading-7 text-gray-900">
+            Analytics overview
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+            Insight from data.
+          </p>
+        </div>
+        <div className="text-xl font-semibold bg-gray-200 px-6 py-3 rounded-2xl">
+          Welcome {currentUser.firstName} 👋
+        </div>
+      </div>
+      <div className="mt-10 flex flex-row flex-wrap gap-8 w-full justify-start ">
+        {dummyData.map(({ key, name, value, href }) => {
+          return (
+            <OverviewCard key={key} name={name} value={value} href={href} />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default overview;
+export default Overview;
